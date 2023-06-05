55cca8f2739e3d09ad8de67a06719f4ea915834d
Ajay Dumaraliya <dumaraliyaajay1912@gmail.com>
2023-06-05 19:05:52 +0530

    phone validation in max-length added

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
 function myfunction() {
     // Name validation...
     let name = document.getElementById('user_name').value;
+    // let correct_way = /^[A-Za-z]+$/;
+
+    // (name == "") ? alert(` ** Please fill user name!`): (name.length < 3) ? alert(` ** User name must be atlest 3 charactor!`) : (name.length > 20) ? alert(` ** User name must be 20 charactor!`) : (name.match(correct_way)) ? true : alert(` ** Only alphabet are allowed!`);
+    // return false;
+
     // If user name is empty...
     if (name == "") {
         document.getElementById('message').innerHTML = ` ** Please fill user name!`;
@@ -25,8 +30,13 @@ function myfunction() {
         return false;
     }
 
+
     // Phone number validation...
     let number = document.getElementById('mobileNumber').value;
+
+    // (number == "") ? alert(` ** Please fill mobile number!`): (isNaN(number)) ? alert(` ** Enter only number!`) : (number.length < 10) ? alert(` ** Mobile number should be 10 digit!`) : (number.length > 10) ? alert(` ** Mobile number should be 10 digit!`) : (number.charAt(0) != 9 && number.charAt(0) != 8 && number.charAt(0) != 7 && number.charAt(0) != 6) ? alert(` ** Mobile number should be start with 9, 8, 7 or 6!`) : console.log('Name :', name, 'Number :', number);
+    // return false;
+
     // If user input Empty number...
     if (number == "") {
         document.getElementById('message1').innerHTML = ` ** Please fill mobile number!`;
@@ -47,5 +57,5 @@ function myfunction() {
     if (number.charAt(0) != 9 && number.charAt(0) != 8 && number.charAt(0) != 7 && number.charAt(0) != 6) {
         document.getElementById('message1').innerHTML = ` ** Mobile number should be start with 9, 8, 7 or 6!`;
         return false;
-    }
+    } 
 }
